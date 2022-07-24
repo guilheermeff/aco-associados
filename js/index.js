e8e92f0de90ffe5d0201d88b9d782f1d113ec8df
Guilherme Fernandes <guilheermeff@outlook.com>
2022-07-24 18:50:39 -0400

    create direito digital page

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ router.add("/direitoTributario", "/pages/direito-tributario.html")
 router.add("/direitoAdministrativo", "/pages/direito-administrativo.html")
 router.add("/direitoTrabalhista", "/pages/direito-trabalhista.html")
 router.add("/direitoPatrimonial", "/pages/direito-patrimonial.html")
+router.add("/direitoDigital", "/pages/direito-digital.html")
 
 router.handle()
 // (find line function) window.route = () => router.route()
